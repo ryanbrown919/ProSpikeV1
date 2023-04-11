77843a0c5824a40feeefce901a6270b0c0a33e62
ryanbrown919 <ryanbrown919@gmail.com>
2023-04-10 19:54:54 -0700

    Actually put good code here

--- ProspikeESP/ProspikeESP.ino
+++ ProspikeESP/ProspikeESP.ino
@@ -18,9 +18,9 @@
           This means the potentiometer step is 78 ohms
 
     Functions: 
-    ds5302.setWiper(value): sets new wiper value
-    ds5302.getWiper(): returns current wiper value
-    ds5302.setWiperDefault(value):  sets the value of wiper when device turns on
+    ds3502.setWiper(value): sets new wiper value
+    ds3502.getWiper(): returns current wiper value
+    ds3502.setWiperDefault(value):  sets the value of wiper when device turns on
 
 */
 /* Stepper Motor
@@ -55,9 +55,16 @@
   
 
 */
+//I2C
+#include <Wire.h>
+
 #include <Adafruit_DS3502.h>
 #include <NewPing.h>
+#include <Servo.h>
+#include <SoftwareSerial.h>
+#include <AccelStepper.h>
 
+Servo reloadServo;
 Adafruit_DS3502 ds3502 = Adafruit_DS3502();
 
 // Linear actuator
@@ -66,98 +73,261 @@ Adafruit_DS3502 ds3502 = Adafruit_DS3502();
 #define R_EN 27 // Forward Enable when HIGH
 #define L_EN 14 // Reverse Enable when HIGH
 
-// Sonar Sensor
-#define ECHO 35 // Sensor input
-#define TRIG 32 // Sensor timing output
-#define MAX_DISTANCE 200 // Max measuring distance of 200 cm is more than we need for out application 
-NewPing sonar(TRIG, ECHO, MAX_DISTANCE); // NewPing setup of pins and max distance
-//long duration;
-//long distance;
+//Arduino UART
+#define RXp2 16
+#define TXp2 17
+
+//Arduino - Arduino UART
+//SoftwareSerial mySerial(6, 7);
+
+//Linear Actuator Home
+#define homePoint 14
+
+
 
 // Stepper motor
-#define DIR 16 // Controls the direction of rotation
-#define STEP 17 // Each rising edge causes a step to be taken
+const int dirPin1 = 2;
+const int stepPin1 = 3; 
+const int enablePin = 4;
+int dist = 630;
+unsigned long timeout = 3000;
+unsigned long startTime;
+AccelStepper stepper(AccelStepper::DRIVER, stepPin1, dirPin1);
 
+int motorSpeed = 0;
+int linAct = homePoint;
+int command;
+char error;
+int oldSpeed = 0;
 
 void setup() {
-
-
-  pinMode(RPWM, OUTPUT);
+  Serial.begin(9600);
+  //mySerial.begin(9600);
+  pinMode(enablePin, OUTPUT);
+  stepper.setMaxSpeed(300);
+  stepper.setAcceleration(200);
+  stepper.setSpeed(40);
+  Wire.begin(8); 
+  reloadServo.attach(9);
+  digitalWrite(enablePin, HIGH);
+  Wire.onReceive(receiveEvent);
+ /* pinMode(RPWM, OUTPUT);
   pinMode(FPWM, OUTPUT);
   pinMode(R_EN, OUTPUT);
   pinMode(L_EN, OUTPUT);
   pinMode(ECHO, INPUT);
-  pinMode(TRIG, OUTPUT);
-  pinMode(DIR, OUTPUT);
-  pinMode(STEP, OUTPUT);
+  pinMode(TRIG, OUTPUT); */
 
-  digitalWrite(stp, LOW);
-  digitalWrite(dir, LOW);
+  reloadServo.write(10);
   
-  Serial.begin(115200);
+  //attachInterrupt(digitalPinToInterrupt(2), interruptRoutine, FALLING);
+  //Serial.println("WAAAA");
+  
   // Wait until serial port is opened
-  while (!Serial) { delay(1); }
-
+  //while (!Serial) { delay(1); }
+  Serial.println("Start");
   if (!ds3502.begin()) {
     Serial.println("Couldn't find DS3502 chip");
     while (1);
   }
   Serial.println("Found DS3502 chip");
 
+  
+  
+    sendLinAct(homePoint);
+
+    //ds3502.setWiperDefault(0);
+
+}
+
+/*void interruptRoutine(){
+  String message = mySerial.readStringUntil('\n');
+  
+}*/
+void reload(){ // Sequence that lowers the stepper enough for a ball to pass
+  reloadServo.write(90);
+  delay(650);
+  reloadServo.write(10);
+  Serial.println("Reload");
+  
+}
+
+void motorSpeedFunc(int newSpeed, int oldSpeed){
+    int tempSpeed = oldSpeed;
+    int step1 = 1;
+    int diff = abs(newSpeed - oldSpeed);
+    if ((newSpeed - oldSpeed) < 0){
+      step1 = -1;
+    }
+    if (newSpeed != 0 && newSpeed - oldSpeed > 0){
+      diff = diff + 8;
+    }
+    for (int i = 0; i <= diff; i++){
+
+      tempSpeed = tempSpeed +  step1;
+      if (tempSpeed < 0){
+        tempSpeed = 0;
+      }
+      ds3502.setWiper(tempSpeed);
+      delay(75);
+    }
+    if (newSpeed != 0 && newSpeed - oldSpeed > 0){
+      for (int d = 8; d > 0; d--){
+        tempSpeed = tempSpeed - 1;
+        ds3502.setWiper(tempSpeed);
+        delay(50);
+      }
+    }
 
   
+}
+void resetFeed(){ // Send the feed down until limit swicth is pressed
+Serial.println("Feed");
+}
 
 
-  
+void stepFunction(){
+  digitalWrite(enablePin, LOW);
+  delay(500);
+
+  Serial.println("Moving up");
+  stepper.move(dist);
+  stepper.runToPosition();
+  delay(1000);
+
+  Serial.println("Moving down");
+  stepper.move(-dist);
+  stepper.runToPosition();
+  delay(1000);
+
+  digitalWrite(enablePin, HIGH);
+}
+void launch(){ // Insert stepper code here
+  stepFunction();
+
+}
+void sendLinAct(int linAct){
+  Serial.println("Transmiting");
+        Wire.beginTransmission(13);
+        Wire.write(linAct);
+        Wire.endTransmission();
+        Serial.println("Done Transmitting");
+}
+void receiveEvent(int bytes) {
+  error = Wire.read();    // read one character from the I2C
+  Serial.print(error);
 }
 
 void loop() {
-
-
-
-
-}
-// Turns the stepper motor forwards
-void stepUp()
-{
-  
-  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
-  for(x= 0; x<5000; x++)  //Loop through for a set amout of steps *WILL NEED TO BE CALIBRATED ON DEVICE*
-  {
-    digitalWrite(stp,HIGH); //Trigger one step forward
-    delay(1);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
-    delay(1);
+  Serial.println("Looping");
+  //error = '';
+  while (!Serial.available()) { // Wait for Serial input
+    delay(100);
+    Serial.println("Waiting");
   }
+   
+   
+//Wait for incoming data
+  //if (Serial.available() > 0) {
+    // Read the incoming data up to the first comma
+    String data = Serial.readStringUntil(',');
+    Serial.println(data);
+    // Convert the first value to an integer
+    motorSpeed = data.toInt();
   
+    // Read the second value up to the newline character
+    data = Serial.readStringUntil('.');
+    Serial.println(data);
+    // Convert the second value to an integer
+    linAct = data.toInt();
+    
+
+    data = Serial.readStringUntil('\n');
+    Serial.println(data);
+    int command = data.toInt();
+    switch (command) {
+      case 1: // 
+      
+          reload();
+          //oldSpeed = motorSpeed;
+      break;
+      case 2: 
+
+          resetFeed();
+      //oldSpeed = motorSpeed;
+
+      break;
+      case 3:
+        launch();
+        //oldSpeed = motorSpeed;
+  
+      break;
+      case 4: // Shutoff sequence
+        sendLinAct(homePoint);
+        motorSpeedFunc(0, oldSpeed);
+        oldSpeed = motorSpeed;
+
+      break;
+      case 5: // Just adjusting the sliders
+        sendLinAct(linAct);
+        motorSpeedFunc(motorSpeed, oldSpeed);
+        //ds3502.setWiper(motorSpeed);
+        
+        oldSpeed = motorSpeed;
+      
+      break;
+      case 6: // Demo Mode
+        if (motorSpeed == 50 && linAct == 16){
+          reload();
+        }
+        sendLinAct(linAct);
+        //ds3502.setWiper(motorSpeed);
+        motorSpeedFunc(motorSpeed,oldSpeed); 
+        startTime = millis();
+        
+        while (error != 'D' || millis() - startTime > timeout){
+          //delay(100);
+        }
+        launch();
+        sendLinAct(homePoint);
+        startTime = millis();
+        
+        while (error != 'D' || millis() - startTime > timeout){
+          //delay(100);
+        }
+        //ds3502.setWiper(0);
+        oldSpeed = motorSpeed;
+      break;
+      case 0: // Code that runs when no commands present, as is the case when it is the automatic sequence
+        reload();
+        sendLinAct(linAct);
+        //ds3502.setWiper(motorSpeed);
+        motorSpeedFunc(motorSpeed,oldSpeed); 
+        
+         startTime = millis();
+        while (error != 'D' || millis() - startTime > timeout){
+          //delay(100);
+        }
+        launch();
+        sendLinAct(homePoint);
+        startTime = millis();
+        
+        while (error != 'D' || millis() - startTime > timeout){
+          //delay(100);
+        }
+        //ds3502.setWiper(0);
+        oldSpeed = motorSpeed;
+        //motorSpeed = 0;
+        //motorSpeedFunc(motorSpeed, oldSpeed);
+        //oldSpeed = motorSpeed;
+        Serial.write("Ready");
+      break;
+    }
+
+    
+   
+    
+  
+  //}
+   
 }
-
-// Turns the stepper motor backwards
-void stepDown()
-{
-  
-  digitalWrite(dir, HIGH); //Pull direction pin HIGH to move "Backwards"
-  for(x= 0; x<5000; x++)  //Loop through for a set amout of steps *WILL NEED TO BE CALIBRATED ON DEVICE*
-  {
-    digitalWrite(stp,HIGH); //Trigger one step forward
-    delay(1);
-    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
-    delay(1);
-  }
-  
-}
-/* Manually measuring distance
- * // Read the sensor value
-int sensorRead(){
-
-  digitalWite(TRIG, LOW);
-  delayMicroseconds(2);
-
-  digitalWite(TRIG, HIGH)
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
-
-  duration = pulsIn(ECHO, HIGH);
-
-  return duration * 0.034/2;
-} */
